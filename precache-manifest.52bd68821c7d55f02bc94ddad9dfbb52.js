59545ccdfd529c2069bf61387b495e3a26e09039
Kotelnikov Igor <digikot1996@gmail.com>
2019-12-11 14:59:00 +0200

    Updates

---
+++ precache-manifest.52bd68821c7d55f02bc94ddad9dfbb52.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e74b09d5a031495157068c75883144e",
+    "url": "/goit-react-hw-04-reader/index.html"
+  },
+  {
+    "revision": "40330db718701d499dce",
+    "url": "/goit-react-hw-04-reader/static/css/main.c74dd1ef.chunk.css"
+  },
+  {
+    "revision": "667bf47345e74243a8db",
+    "url": "/goit-react-hw-04-reader/static/js/2.cc9ba706.chunk.js"
+  },
+  {
+    "revision": "40330db718701d499dce",
+    "url": "/goit-react-hw-04-reader/static/js/main.f0769422.chunk.js"
+  },
+  {
+    "revision": "c6419865b813505d5552",
+    "url": "/goit-react-hw-04-reader/static/js/runtime-main.deb7d762.js"
+  }
+]);
